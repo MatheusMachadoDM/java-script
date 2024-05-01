ed2de71867b267847a20e34814f801f2db214de7
MatheusMachadoDM <machadomatheus2906@gmail.com>
2024-05-01 14:41:43 -0300

    Arrumando pastas

---
+++ aula003/ex001/ex001.js
@@ -0,0 +1,7 @@
+var vel = 40
+
+console.log(`Sua velocidade é de ${vel} Km/h, lembre que o limite de velocidade é de 50 Km/h`)
+
+if (vel > 50){
+    console.log(`Você ultrapassou o limite, MULTADO!`)
+}
