10d21c3424b3080b0f343f08302cdd8f2772912d
MatheusMachadoDM <machadomatheus2906@gmail.com>
2024-05-18 10:30:36 -0300

    update

--- exercicios/ex005/script.js
+++ exercicios/ex005/script.js
@@ -6,7 +6,7 @@ var n =[]
 
 function add(){
 
-    if(num.value.length == 0 || num.value > 100 || num.value < 0){
+    if(num.value.length == 0 || num.value > 100 || num.value < 1){
         window.alert('[ERRO] Você não cumpriu alguma das condições!')
     }else{
         n.push(Number(num.value))
@@ -19,9 +19,19 @@ function add(){
 }
 
 function finalizar(){
-
+    //Quantidade de números dentro da array
     res.innerHTML = `Ao todo, temos ${n.length} números cadastrados! <br>`
-    res.innerHTML += `${n.}`
+  
+    //Maior valor dentro da array
+    var maior = 0
+    for(var c = 0; c < n.length; c++){
+        if(n[c] > maior){
+            maior = n[c]
+        }
+    }
+    res.innerHTML += `O maior número é ${maior}!`
+
+    //Menor valor
 }
 
 addEventListener("keydown", function(event){
